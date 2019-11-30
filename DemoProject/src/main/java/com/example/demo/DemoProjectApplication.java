d8e966d1338571db1b0266c90de4d634427a9da9
krish <17404467+krishkishan@users.noreply.github.com>
2019-11-30 18:24:09 +0530

    MavenProject

---
+++ DemoProject/src/main/java/com/example/demo/DemoProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println(" maven project executed successfully ");
+		SpringApplication.run(DemoProjectApplication.class, args);
+	}
+
+}
